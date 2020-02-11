eb55d7f1676eeae5ef2b0d6d5a7c04d9be148967
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-02-11 11:45:06 +0530

    ADDED: Reset The Game Board

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
+readonly ROWS=9
+declare -a gameBoard
 echo "Welcome To Tic Tac Toe Game"
+#resetTheBoard: To initialize gameBoard or for fresh start 
+resetTheBoard()
+{
+	for ((i=0;i<$ROWS;i++))
+	do
+		gameBoard[$i]="-"
+	done
+}
+resetTheBoard
